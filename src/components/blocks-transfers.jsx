852502bfa3064c3213a13f076fae3afc4368a25d
John Otu <johnne01@gmail.com>
2021-12-22 10:51:09 +0100

    feat: home screen latest blocks
    implemented latest block components.
    * retrieved best header via subscription
    * obtained number of extrinsics and events for a particular block hash

---
+++ src/components/blocks-transfers.jsx
@@ -0,0 +1,141 @@
+export default function BlocksTransfers() {
+  return (
+    <div className="transfers">
+      <div className="d-flex justify-content-space-between">
+        <div className="d-flex align-items-center">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M17.5602 1.4963C17.8385 1.21804 18.2896 1.21803 18.5679 1.49627L22.5036 5.43173C22.6374 5.56549 22.7125 5.74694 22.7123 5.93611C22.7122 6.12527 22.6368 6.30661 22.5028 6.44016L18.5671 10.3637C18.2884 10.6415 17.8373 10.6408 17.5595 10.3621C17.2817 10.0835 17.2824 9.63232 17.561 9.35451L20.9914 5.9348L17.5603 2.50393C17.282 2.22569 17.282 1.77456 17.5602 1.4963Z"
+              fill="#706CFF"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M2.28748 5.93418C2.28748 5.54068 2.60647 5.22168 2.99998 5.22168H22C22.3935 5.22168 22.7125 5.54068 22.7125 5.93418C22.7125 6.32768 22.3935 6.64668 22 6.64668H2.99998C2.60647 6.64668 2.28748 6.32768 2.28748 5.93418Z"
+              fill="#706CFF"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M7.44045 12.6395C7.71819 12.9183 7.71738 13.3694 7.43863 13.6472L4.00849 17.0649L7.43956 20.4963C7.7178 20.7746 7.71777 21.2257 7.43951 21.504C7.16125 21.7822 6.71012 21.7822 6.43188 21.5039L2.49614 17.5678C2.36237 17.434 2.2873 17.2525 2.28748 17.0633C2.28765 16.8741 2.36306 16.6928 2.49707 16.5593L6.43282 12.6377C6.71157 12.36 7.1627 12.3608 7.44045 12.6395Z"
+              fill="#706CFF"
+            />
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M2.28748 17.0658C2.28748 16.6723 2.60647 16.3533 2.99998 16.3533H22C22.3935 16.3533 22.7125 16.6723 22.7125 17.0658C22.7125 17.4593 22.3935 17.7783 22 17.7783H2.99998C2.60647 17.7783 2.28748 17.4593 2.28748 17.0658Z"
+              fill="#706CFF"
+            />
+          </svg>
+
+          <h3 className="ml-3">Transfers</h3>
+        </div>
+        <a href="/transfers" className="button small">
+          All
+        </a>
+      </div>
+      <div>
+        <table className="table">
+          <tbody>
+            <tr>
+              <td className="text-large">
+                <div>
+                  Extrinsic index#{" "}
+                  <span className="text-accent-purple">787,89,21</span>
+                </div>
+                <div>
+                  <span className="text-dark-white text-small">From</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>{" "}
+                  <span className="text-dark-white text-small">To</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>
+                </div>
+              </td>
+              <td>
+                <div className="text-dark-white text-small">4 secs ago</div>
+                <div className="text-accent-purple text-small">3,5 PEAQ</div>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-large">
+                <div>
+                  Extrinsic index#{" "}
+                  <span className="text-accent-purple">787,89,21</span>
+                </div>
+                <div>
+                  <span className="text-dark-white text-small">From</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>{" "}
+                  <span className="text-dark-white text-small">To</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>
+                </div>
+              </td>
+              <td>
+                <div className="text-dark-white text-small">4 secs ago</div>
+                <div className="text-accent-purple text-small">3,5 PEAQ</div>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-large">
+                <div>
+                  Extrinsic index#{" "}
+                  <span className="text-accent-purple">787,89,21</span>
+                </div>
+                <div>
+                  <span className="text-dark-white text-small">From</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>{" "}
+                  <span className="text-dark-white text-small">To</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>
+                </div>
+              </td>
+              <td>
+                <div className="text-dark-white text-small">4 secs ago</div>
+                <div className="text-accent-purple text-small">3,5 PEAQ</div>
+              </td>
+            </tr>
+            <tr>
+              <td className="text-large">
+                <div>
+                  Extrinsic index#{" "}
+                  <span className="text-accent-purple">787,89,21</span>
+                </div>
+                <div>
+                  <span className="text-dark-white text-small">From</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>{" "}
+                  <span className="text-dark-white text-small">To</span>{" "}
+                  <span className="text-accent-purple text-small">
+                    0xcd2d8b2cb...
+                  </span>
+                </div>
+              </td>
+              <td>
+                <div className="text-dark-white text-small">4 secs ago</div>
+                <div className="text-accent-purple text-small">3,5 PEAQ</div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
